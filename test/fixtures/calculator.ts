035b088aa64afe73ff410ba5518037e24152f6cc
Misha Kaletsky <mmkal@users.noreply.github.com>
2025-06-09 17:27:59 -0400

    "

--- test/fixtures/calculator.ts
+++ test/fixtures/calculator.ts
@@ -1,23 +1,22 @@
-import {trpcServer, TrpcCliMeta, createCli, z} from '../../src'
-
+import {trpcServer, TrpcCliMeta, createCli, z} from "trpc-cli"
 const t = trpcServer.initTRPC.meta<TrpcCliMeta>().create()
 
 const router = t.router({
   sayHello: t.procedure
     .input(
       z.tuple([
-        z.string().describe('name'),
+        z.string().describe("name"),
         z.object({
-          enthusiasm: z.number().describe('exclamation marks'),
-        }),
-      ]),
+          enthusiasm: z.number().describe("exclamation marks"),
+        })
+      ])
     )
     .query(({input}) => {
       const [name, {enthusiasm}] = input
-      return `Hello ${name}` + '!'.repeat(enthusiasm)
-    }),
+      return `Hello ${name}` + !.repeat(enthusiasm)
+    })
 })
 
 const cli = createCli({router})
 
-cli.run()
+void cli.run()
