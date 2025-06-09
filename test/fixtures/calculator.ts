35893dfa3bce6822fc11a410fa9a5d264c288256
Misha Kaletsky <mmkal@users.noreply.github.com>
2025-06-09 17:25:31 -0400

    forgot compile

--- test/fixtures/calculator.ts
+++ test/fixtures/calculator.ts
@@ -1,56 +1,23 @@
-import {z} from 'zod'
-import {createCli, type TrpcCliMeta, trpcServer} from '../../src'
+import {trpcServer, TrpcCliMeta, createCli, z} from '../../src'
 
-const trpc = trpcServer.initTRPC.meta<TrpcCliMeta>().create()
+const t = trpcServer.initTRPC.meta<TrpcCliMeta>().create()
 
-const router = trpc.router({
-  add: trpc.procedure
-    .meta({
-      description:
-        'Add two numbers. Use this if you and your friend both have apples, and you want to know how many apples there are in total.',
-    })
-    .input(z.tuple([z.number(), z.number()]))
-    .query(({input}) => input[0] + input[1]),
-  subtract: trpc.procedure
-    .meta({
-      description: 'Subtract two numbers. Useful if you have a number and you want to make it smaller.',
-    })
-    .input(z.tuple([z.number(), z.number()]))
-    .query(({input}) => input[0] - input[1]),
-  multiply: trpc.procedure
-    .meta({
-      description:
-        'Multiply two numbers together. Useful if you want to count the number of tiles on your bathroom wall and are short on time.',
-    })
-    .input(z.tuple([z.number(), z.number()]))
-    .query(({input}) => input[0] * input[1]),
-  divide: trpc.procedure
-    .meta({
-      version: '1.0.0',
-      description:
-        "Divide two numbers. Useful if you have a number and you want to make it smaller and `subtract` isn't quite powerful enough for you.",
-      examples: 'divide --left 8 --right 4',
-    })
+const router = t.router({
+  sayHello: t.procedure
     .input(
       z.tuple([
-        z.number().describe('numerator'),
-        z
-          .number()
-          .refine(n => n !== 0)
-          .describe('denominator'),
+        z.string().describe('name'),
+        z.object({
+          enthusiasm: z.number().describe('exclamation marks'),
+        }),
       ]),
     )
-    .mutation(({input}) => input[0] / input[1]),
-  squareRoot: trpc.procedure
-    .meta({
-      description:
-        'Square root of a number. Useful if you have a square, know the area, and want to find the length of the side.',
-    })
-    .input(z.number())
     .query(({input}) => {
-      if (input < 0) throw new Error(`Get real`)
-      return Math.sqrt(input)
+      const [name, {enthusiasm}] = input
+      return `Hello ${name}` + '!'.repeat(enthusiasm)
     }),
 })
 
-void createCli({router}).run()
+const cli = createCli({router})
+
+cli.run()
