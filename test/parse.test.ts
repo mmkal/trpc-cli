955dd2515254d4dcfa9fd74819ebeb64ea0d165b
Misha Kaletsky <mmkal@users.noreply.github.com>
2025-06-10 10:36:13 -0400

    test for optional positional
    this was a regression - e2e tests actually caught it but i blindly updated the assertion 😬

--- test/parse.test.ts
+++ test/parse.test.ts
@@ -1,6 +1,6 @@
 import {initTRPC} from '@trpc/server'
 import {expect, test, vi} from 'vitest'
-import {createCli, TrpcCliMeta, z} from '../src'
+import {createCli, TrpcCli, TrpcCliMeta, z} from '../src'
 import {FailedToExitError} from '../src/errors'
 
 const t = initTRPC.meta<TrpcCliMeta>().create()
@@ -17,21 +17,7 @@ test('default command', async () => {
 
   const cli = createCli({router})
 
-  const run = async (argv: string[]) => {
-    const exit = vi.fn() as any
-    const log = vi.fn()
-    const result = await cli
-      .run({
-        argv,
-        process: {exit}, // prevent process.exit
-        logger: {info: log},
-      })
-      .catch(err => err)
-    if (result.exitCode !== 0) throw result.cause
-    return {exit, log, result}
-  }
-
-  const runFoo = await run(['foo', '--bar', '1'])
+  const runFoo = await run(cli, ['foo', '--bar', '1'])
 
   expect(runFoo.exit).toHaveBeenCalledWith(0)
   expect(runFoo.log).toHaveBeenCalledWith('{"bar":1}')
@@ -39,7 +25,7 @@ test('default command', async () => {
   expect(runFoo.result.exitCode).toBe(0)
   expect(runFoo.result.cause).toBe('{"bar":1}')
 
-  const runDefault = await run(['--bar', '1'])
+  const runDefault = await run(cli, ['--bar', '1'])
 
   expect(runDefault.exit).toHaveBeenCalledWith(0)
   expect(runDefault.log).toHaveBeenCalledWith('{"bar":1}')
@@ -47,3 +33,71 @@ test('default command', async () => {
   expect(runDefault.result.exitCode).toBe(0)
   expect(runDefault.result.cause).toBe('{"bar":1}')
 })
+
+test('optional positional', async () => {
+  const router = t.router({
+    foo: t.procedure
+      .input(
+        z.tuple([
+          z.string().optional().describe('name'),
+          z.object({
+            bar: z.number().optional().describe('bar'),
+          }),
+        ]),
+      )
+      .query(({input}) => JSON.stringify(input)),
+  })
+
+  const cli = createCli({router})
+
+  expect(await output(cli, ['foo', 'abc', '--bar', '1'])).toMatchInlineSnapshot(`"["abc",{"bar":1}]"`)
+  expect(await output(cli, ['foo', '--bar', '1'])).toMatchInlineSnapshot(`"[null,{"bar":1}]"`)
+  expect(await output(cli, ['foo'])).toMatchInlineSnapshot(`"[null,{}]"`)
+  expect(await output(cli, ['foo', 'def'])).toMatchInlineSnapshot(`"["def",{}]"`)
+})
+
+test('required positional', async () => {
+  const router = t.router({
+    foo: t.procedure
+      .input(
+        z.tuple([
+          z.string().describe('name'),
+          z.object({
+            bar: z.number().optional().describe('bar'),
+          }),
+        ]),
+      )
+      .query(({input}) => JSON.stringify(input)),
+  })
+
+  const cli = createCli({router})
+
+  expect(await output(cli, ['foo', 'abc', '--bar', '1'])).toMatchInlineSnapshot(`"["abc",{"bar":1}]"`)
+  expect(await output(cli, ['foo', '--bar', '1'])).toMatchInlineSnapshot(
+    `"CommanderError: error: missing required argument 'name'"`,
+  )
+  expect(await output(cli, ['foo'])).toMatchInlineSnapshot(`"CommanderError: error: missing required argument 'name'"`)
+  expect(await output(cli, ['foo', 'def'])).toMatchInlineSnapshot(`"["def",{}]"`)
+})
+
+const run = async (cli: TrpcCli, argv: string[]) => {
+  const exit = vi.fn() as any
+  const log = vi.fn()
+  const result = await cli
+    .run({
+      argv,
+      process: {exit}, // prevent process.exit
+      logger: {info: log},
+    })
+    .catch(err => err)
+  if (result.exitCode !== 0) throw result.cause
+  return {exit, log, result}
+}
+const output = async (cli: TrpcCli, argv: string[]) => {
+  try {
+    const {log} = await run(cli, argv)
+    return log.mock.calls.map(call => call[0]).join('\n')
+  } catch (err) {
+    return String(err)
+  }
+}
