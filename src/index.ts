701e09f3098692b82fc1960ed93dfadb483a140a
Misha Kaletsky <mmkal@users.noreply.github.com>
2025-06-10 23:42:46 -0400

    rm lint suppressions

--- src/index.ts
+++ src/index.ts
@@ -447,8 +447,8 @@ export function createCli<R extends AnyRouter>({router, ...params}: TrpcCliParam
         } else if (isOrpcRouter(router)) {
           // eslint-disable-next-line @typescript-eslint/no-require-imports
           const {call} = require('@orpc/server') as typeof import('@orpc/server')
-          // eslint-disable-next-line @typescript-eslint/no-unsafe-return, @typescript-eslint/no-shadow, @typescript-eslint/no-explicit-any
-          caller = {[procedurePath]: (input: any) => call(procedure as never, input, {context: params.context})}
+          // create an object which acts enough like a trpc caller to be used for this specific procedure
+          caller = {[procedurePath]: (_input: unknown) => call(procedure as never, _input, {context: params.context})}
         } else {
           const createCallerFactor = resolvedTrpcServer.initTRPC.create().createCallerFactory as CreateCallerFactoryLike
           caller = createCallerFactor(router)(params.context)
