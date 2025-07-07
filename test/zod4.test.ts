a5309d61daf16b4c61be131faecc4afe048ec406
Misha Kaletsky <mmkal@users.noreply.github.com>
2025-07-07 14:34:12 -0400

    negateBooleans test

--- test/zod4.test.ts
+++ test/zod4.test.ts
@@ -671,4 +671,27 @@ test('negatable boolean', async () => {
   expect(await run(router, ['test', '--foo', 'false'])).toMatchInlineSnapshot(`"{ foo: false }"`)
 })
 
+test('default negatable boolean', async () => {
+  const router = t.router({
+    test: t.procedure
+      .meta({negateBooleans: true})
+      .input(z.object({foo: z.boolean(), bar: z.boolean().meta({negatable: false})}))
+      .query(({input}) => `${inspect(input)}`),
+  })
+
+  expect(await run(router, ['test', '--foo'])).toMatchInlineSnapshot(`"{ foo: true, bar: false }"`)
+  expect(await run(router, ['test', '--no-foo'])).toMatchInlineSnapshot(`"{ foo: false, bar: false }"`)
+  expect(await run(router, ['test', '--foo', 'true'])).toMatchInlineSnapshot(`"{ foo: true, bar: false }"`)
+  expect(await run(router, ['test', '--foo', 'false'])).toMatchInlineSnapshot(`"{ foo: false, bar: false }"`)
+
+  expect(await run(router, ['test', '--bar'])).toMatchInlineSnapshot(`"{ foo: false, bar: true }"`)
+  await expect(run(router, ['test', '--no-bar'])).rejects.toMatchInlineSnapshot(`
+    CLI exited with code 1
+      Caused by: CommanderError: error: unknown option '--no-bar'
+    (Did you mean one of --bar, --no-foo?)
+  `)
+  expect(await run(router, ['test', '--bar', 'true'])).toMatchInlineSnapshot(`"{ foo: false, bar: true }"`)
+  expect(await run(router, ['test', '--bar', 'false'])).toMatchInlineSnapshot(`"{ foo: false, bar: false }"`)
+})
+
 // todo: either create a meta registry or use module augmentation to allow adding aliases for options etc.
