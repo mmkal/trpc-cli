f4aea62ee0dec7c870d8b0d14a9058e51bb45b9b
Misha Kaletsky <15040698+mmkal@users.noreply.github.com>
2025-07-11 17:14:25 -0400

    let's see if we can just ts-ignore in the .ts file (updated 17:21)

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,3 @@
-/// <reference types="./module-augmentation.d.ts" />
-
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import * as trpcServer11 from '@trpc/server'
 import {Argument, Command as BaseCommand, InvalidArgumentError, InvalidOptionArgumentError, Option} from 'commander'
@@ -32,6 +30,25 @@ import {
 } from './trpc-compat'
 import {ParsedProcedure, TrpcCli, TrpcCliMeta, TrpcCliParams, TrpcCliRunParams} from './types'
 import {looksLikeInstanceof} from './util'
+
+// @ts-ignore - zod might not be installed. oh well, you still get this one interface
+declare module 'zod/v4' {
+  export interface GlobalMeta {
+    /**
+     * If true, this property will be mapped to a positional CLI argument by trpc-cli. Only valid for string, number, or boolean types (or arrays of these types).
+     * Note: the order of positional arguments is determined by the order of properties in the schema.
+     * For example, the following are different:
+     * - `z.object({abc: z.string().meta({positional: true}), xyz: z.string().meta({positional: true})})`
+     * - `z.object({xyz: z.string().meta({positional: true}), abc: z.string().meta({positional: true})})`
+     */
+    positional?: boolean
+    /**
+     * If set, this value will be used an alias for the option.
+     * Note: this is only valid for options, not positional arguments.
+     */
+    alias?: string
+  }
+}
 
 export * from './types'
 
