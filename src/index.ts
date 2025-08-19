2d1d35cdbe61a9a675a01f22e6f254d49824a6fd
Misha Kaletsky <15040698+mmkal@users.noreply.github.com>
2025-08-19 10:13:53 +0100

    augment `zod` as well as `zod/v4`?

--- src/index.ts
+++ src/index.ts
@@ -49,6 +49,25 @@ declare module 'zod/v4' {
   }
 }
 
+// @ts-ignore zod is an optional peer dependency so might not be installed. oh well, you still get this one interface
+declare module 'zod' {
+  export interface GlobalMeta {
+    /**
+     * If true, this property will be mapped to a positional CLI argument by trpc-cli. Only valid for string, number, or boolean types (or arrays of these types).
+     * Note: the order of positional arguments is determined by the order of properties in the schema.
+     * For example, the following are different:
+     * - `z.object({abc: z.string().meta({positional: true}), xyz: z.string().meta({positional: true})})`
+     * - `z.object({xyz: z.string().meta({positional: true}), abc: z.string().meta({positional: true})})`
+     */
+    positional?: boolean
+    /**
+     * If set, this value will be used an alias for the option.
+     * Note: this is only valid for options, not positional arguments.
+     */
+    alias?: string
+  }
+}
+
 export * from './types'
 
 /** @deprecated use `import * as trpcServer from '@trpc/server'` instead */
