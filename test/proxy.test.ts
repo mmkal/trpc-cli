3ede6b551cd2b601723356e98d25d8e7a4a388f3
Misha Kaletsky <15040698+mmkal@users.noreply.github.com>
2025-11-15 23:24:08 +0000

    [experimental] proxify helper to create a cli for a remote server (#153)
    allow "proxifying" a router (for want of a better word for this)
    
    takes an existing router, but instead of calling its procedures
    directly, a clone is made, where the implementation uses a trpc client
    to hit a running server instead. You can create the client however you
    want, but usually it'll be via `createClient` like:
    
    ```ts
    proxify(router, async () => {
      return createTRPCClient<typeof router>({
        links: [httpLink({url: 'http://localhost:7500'})],
      })
    })
    ```
    
    e.g. in proxy.test.ts, with a server running on port 7500:
    
    ```ts
    test('proxy', async () => {
      const proxiedRouter = proxify(router, async () => {
        return createTRPCClient<typeof router>({
          links: [httpLink({url: 'http://localhost:7500'})],
        })
      })
      // procedures and input types match that of `router`, but the "implementation" of each procedure is to hit the remote server over http
      expect(await run(proxiedRouter, ['greeting', '--name', 'Bob'])).toMatchInlineSnapshot(`"Hello Bob"`)
      expect(await run(proxiedRouter, ['deeply', 'nested', 'farewell', '--name', 'Bob'])).toMatchInlineSnapshot(
        `"Goodbye Bob"`,
      )
    })
    ```

---
+++ test/proxy.test.ts
@@ -0,0 +1,73 @@
+import {createTRPCClient, httpLink} from '@trpc/client'
+import {initTRPC} from '@trpc/server'
+import {createHTTPServer} from '@trpc/server/adapters/standalone'
+import {afterAll, beforeAll, expect, test} from 'vitest'
+import {z} from 'zod'
+import {TrpcCliMeta} from '../src/index.js'
+import {proxify} from '../src/proxify.js'
+import {run} from './test-run.js'
+
+const t = initTRPC.meta<TrpcCliMeta>().create()
+
+const router = t.router({
+  greeting: t.procedure
+    .input(
+      z.object({
+        name: z.string(),
+      }),
+    )
+    .query(({input}) => `Hello ${input.name}`),
+  deeply: {
+    nested: {
+      farewell: t.procedure
+        .input(
+          z.object({
+            name: z.string(),
+          }),
+        )
+        .query(({input}) => `Goodbye ${input.name}`),
+    },
+  },
+})
+
+const runServer = async () => {
+  const server = createHTTPServer({router})
+  server.listen(7500)
+
+  const client = createTRPCClient<typeof router>({
+    links: [httpLink({url: 'http://localhost:7500'})],
+  })
+  for (let i = 0; i <= 10; i++) {
+    const success = await client.greeting.query({name: 'Bob'}).then(
+      r => !!r,
+      () => false,
+    )
+    if (success) break
+    if (i === 10) throw new Error('Failed to connect to server')
+    if (!success) continue
+  }
+
+  return server
+}
+
+let server: Awaited<ReturnType<typeof runServer>>
+
+beforeAll(async () => {
+  server = await runServer()
+})
+
+afterAll(async () => {
+  server.close()
+})
+
+test('proxy', async () => {
+  const proxiedRouter = proxify(router, async () => {
+    return createTRPCClient<typeof router>({
+      links: [httpLink({url: 'http://localhost:7500'})],
+    })
+  })
+  expect(await run(proxiedRouter, ['greeting', '--name', 'Bob'])).toMatchInlineSnapshot(`"Hello Bob"`)
+  expect(await run(proxiedRouter, ['deeply', 'nested', 'farewell', '--name', 'Bob'])).toMatchInlineSnapshot(
+    `"Goodbye Bob"`,
+  )
+})
